eed02a7498e3fbd2d3a94a072a7ece3705e440bf
InsaDams <damiengoron@yahoo.fr>
2020-06-19 15:14:40 +0200

    Creation Classe CMoteur

---
+++ Qualite_Logicielle/CMoteur.cpp
@@ -0,0 +1,22 @@
+#include "CMoteur.h"
+
+void CMoteur::ActiveMoteur(Controleur_Deplacement::MOVE move)
+{
+	switch (move)
+	{
+	case Controleur_Deplacement::UP :
+
+			break;
+	case Controleur_Deplacement::DOWN:
+
+		break;
+	case Controleur_Deplacement::LEFT:
+
+		break;
+	case Controleur_Deplacement::RIGHT:
+
+		break;
+	default:
+		break;
+	}
+}
---
+++ Qualite_Logicielle/CMoteur.h
@@ -0,0 +1,16 @@
+#include <string>
+#include "Controleur_Deplacement.h"
+using namespace std;
+// V1
+#pragma once
+class CMoteur
+{
+public:
+	// Constructeurs
+	CMoteur();
+	void ActiveMoteur(Controleur_Deplacement::MOVE move);
+
+	//Accesseurs et mutateurs
+
+
+};
